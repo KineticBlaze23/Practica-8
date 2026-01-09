f8be49818620b84c2bd500a2a565ae177a49cad7
Fabio Pillajo <114030717+KineticBlaze23@users.noreply.github.com>
2026-01-08 21:40:52 -0500

    Add select menu example in select.sh

--- Scripts/select.sh
+++ Scripts/select.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+# Ejemplo 11
+select seleccion in op1 op2 op3; do
+    if [ $seleccion ]; then
+        echo "Opcion elegida: $seleccion"
+        break
+    else
+        echo "Eleccion incorrecta"
+    fi
+done
