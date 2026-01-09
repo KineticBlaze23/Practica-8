c42482f159f5970296e34f5da29234d89ba7a2c6
Fabio Pillajo <114030717+KineticBlaze23@users.noreply.github.com>
2026-01-08 21:38:54 -0500

    Create cate.sh for user input prompts
    Add a script that prompts for a number and an option.

--- Scripts/cate.sh
+++ Scripts/cate.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# Ejemplo 7
+echo "Escriba un numero del 1 al 10"
+read x
+case $x in
+    1) echo "Escribio uno" ;;
+    2) echo "Escribio 2" ;;
+    *) echo "Me rindo" ;;
+esac
 
+echo "Escriba s o S para SI, n o N para no, o cualquier letra"
+read opcion
+case $opcion in
+    s|S) echo "pulso la opcion SI" ;;
+    n|N) echo "pulso la opcion NO" ;;
+    *) echo "Usted esta indeciso" ;;
+esac
